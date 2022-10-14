6e29f2e97114c68e2528e49258b5a0e920ed6cd8
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-10-14 10:42:32 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,19 +6,16 @@ radio.onReceivedString(function (receivedString) {
     basic.pause(1000)
     debute = 2
 })
-function donnees () {
-    radio.sendValue("x", input.acceleration(Dimension.X))
-    radio.sendValue("y", input.acceleration(Dimension.Y))
-    radio.sendValue("z", input.acceleration(Dimension.Z))
-    led.toggle(0, 0)
-}
 let debute = 0
 radio.setGroup(1)
 debute = 0
 basic.showIcon(IconNames.No)
 basic.forever(function () {
     while (debute == 2) {
-        donnees()
+        radio.sendValue("x", input.acceleration(Dimension.X))
+        radio.sendValue("y", input.acceleration(Dimension.Y))
+        radio.sendValue("z", input.acceleration(Dimension.Z))
+        led.toggle(0, 0)
         basic.pause(2000)
     }
 })
