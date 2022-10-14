3d0c4eddbb42dd9c838da94f5187c2958e33d678
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-10-14 10:23:39 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    control.reset()
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "go") {
+        basic.showIcon(IconNames.Yes)
+        basic.pause(1000)
+        basic.clearScreen()
+        debute = 2
+    }
+})
+let debute = 0
+radio.setGroup(1)
+basic.showIcon(IconNames.No)
 basic.forever(function () {
-	
+    radio.sendValue("x", input.acceleration(Dimension.X))
+    radio.sendValue("y", input.acceleration(Dimension.Y))
+    radio.sendValue("z", input.acceleration(Dimension.Z))
+    led.unplot(0, 0)
+    basic.pause(2000)
 })
