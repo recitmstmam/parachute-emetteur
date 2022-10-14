741a8c61885454ba2cb2ebdfe854be32ffd6c532
Marc-André Mercier <75087183+recitmstmam@users.noreply.github.com>
2022-10-14 10:57:48 -0400

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,7 +14,5 @@ basic.forever(function () {
         radio.sendValue("x", input.acceleration(Dimension.X))
         radio.sendValue("y", input.acceleration(Dimension.Y))
         radio.sendValue("z", input.acceleration(Dimension.Z))
-        led.toggle(0, 0)
-        basic.pause(2000)
     }
 })
